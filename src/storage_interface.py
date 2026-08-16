ba08e30a9a6e22eaecefc534e6b19359ed4ba89d
WellitonT <wellingtontiene@gmail.com>
2026-08-16 17:22:35 -0300

    feat: fecha Fase 1 (OOP avancado) e adiciona pathlib em StorageInterface

--- src/storage_interface.py
+++ src/storage_interface.py
@@ -1,33 +1,16 @@
 from abc import ABC, abstractmethod
 from src.models import Produto
+from pathlib import Path
 import json
 
 class StorageInterface(ABC):
     def __init__(self, caminho: str):
-        self.caminho = caminho
+        self.caminho = Path(caminho)
 
     @abstractmethod
     def salvar(self, produtos: list) -> None:
         ...
 
+    @abstractmethod
     def carregar(self) -> list:
-        ...
-
-class JsonStorage(StorageInterface):
-    def salvar(self, produtos: list) -> None:
-        lista_catalogo = []
-        for produto in produtos:
-            lista_catalogo.append({"nome": produto.nome, "valor": produto.valor})
-        with open(self.caminho, "w") as f:
-            json.dump(lista_catalogo, f, indent=4)
-
-    def carregar(self) -> list:
-        try:
-            with open(self.caminho) as f:
-                dados = json.load(f)
-            lista_dados = []
-            for dado in dados:
-                lista_dados.append(Produto(dado["nome"], dado["valor"]))
-            return lista_dados
-        except FileNotFoundError:
-            return []                     
+        ...                   
